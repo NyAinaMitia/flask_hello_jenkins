e275eaf794b130bd40eb1f78789ef984bb02898b
NyAinaMitia <mitiarazafimanantsoa@gmail.com>
2026-06-11 01:48:32 +0300

    fix route hello manquante

--- app.py
+++ app.py
@@ -7,6 +7,10 @@ app = Flask(__name__)
 def hello_world():
     return 'Hello World!\n'
 
+@app.route('/hello/<username>')
+def hello_user(username):
+    return 'Hello %s!\n' % username
+
 @app.route('/feature/<username>')
 def feature_user(username):
     return 'Hello %s!\n' % username
